19962bec725eab8f1ca136a8d55773a9a64372b3
Sam Thabet <sallam.thabet@gmail.com>
2022-03-05 17:34:22 -0500

    added symbols for generator

--- Develop/script3.js
+++ Develop/script3.js
@@ -10,7 +10,7 @@ function writePassword() {
     return "Sorry, we are unable to process your request. Please try again!";
   } else {
     window.alert("Thank you");
-  }
+  } //TODO split into two functions where write password will initiate the length if it does not pass it will stop or if it does pass it will go on to the next function. So need to focus on the else statement, possible solutions is to remove the window alert or thank you message and have it call another function. That is if we are unable to do both.
   console.log(length);
   //Logging if lower case characters are wanted & Function
 
@@ -18,57 +18,52 @@ function writePassword() {
     "Would you like there to be lower case characters?"
   );
   //TODO need to add an array over here to insert the lower randomized letters
-  function lowerCase(lower) { //TODO need to add a forloop within the function to insert items into the array.
+  function lowerCase(lower) {
+    //TODO need to add a forloop within the function to insert items into the array.
     var low = lower ? Math.floor(Math.random() * 26) + 97 : "";
     return String.fromCharCode(low);
   }
-  
+
   console.log(lowerCase(lower));
+
+  
   //Logging if upper case characters are wanted & Function
 
   var upper = window.confirm(
     "Would you like there to be upper case characters? "
   );
-  if (upper) {
-    function getUpper() {
-      return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-    }
-  } else {
-    function getUpper() {
-      return "";
-    }
+  function upperCase(upper) {
+    var up = upper ? Math.floor(Math.random() * 26) + 65 : "";
+    return String.fromCharCode(up);
   }
-  console.log(upper);
+  
+  console.log(upperCase(upper));
+
+
   //Logging if numbers are wanted
 
   var number = window.confirm("Would you like there to be numbers? ");
-  if (number) {
-    function getNumber() {
-      return String.fromCharCode(Math.floor(Math.random() * 26 + 65));
-    }
-  } else {
-    function getNumber() {
-      return "";
-    }
+  function num(number) {
+    var digit = number ? Math.floor(Math.random() * (99 + 1)) : "";
+    return digit;
   }
-  console.log(number);
+
+  console.log(num(number));
+
+
+
   //Random Symbol & Function
 
   var symbols = window.confirm(
     "Would you like there to be random symbols added to the password? "
   );
-  if (symbols) {
-    function getSymbols() {
-      const symbols = "!@#$%^&*/,.";
-      const symbolspass = [Math.floor(Math.random() * symbols)];
-      console.log(symbolspass);
-      return symbolspass;
-    }
-  } else {
-    function symbols() {
-      return "";
-    }
+  function getSymbol(symbols) {
+    var sym = ["!", "@", "#", "$", "%", "^", "&", "*", "/", "."];
+    const symbolspass = [Math.floor(Math.random() * (9 + 1))];
+    return sym[symbolspass];
   }
+  console.log(getSymbol(symbols));
+  
 }
 // Write password to the #password input
 // function writePassword() {
